75dbce241972815eba61d0957ab379dfacd8487a
FirasBDarwish <94559551+FirasBDarwish@users.noreply.github.com>
2023-07-05 18:52:55 +0000

    added second version of iPP

---
+++ Algorithm Design Implementations/Greedy/Interval Scheduling Problems/Interval Partioning Problem (using self-built priority queue).cpp
@@ -0,0 +1,8 @@
+/*
+
+This is from section 5.1 from Algorithm Design as extensions of the interval scheduling problem. Pseucode located there. 
+
+Primarily using: https://github.com/kartikkukreja/blog-codes/blob/master/src/Interval%20Partitioning%20Problem.cpp
+
+*/
+
