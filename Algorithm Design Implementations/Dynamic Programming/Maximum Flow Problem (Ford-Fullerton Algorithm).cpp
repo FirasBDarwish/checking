eb11fe5394bc594c82cd55774b98b9ff3e0562c4
FirasBDarwish <94559551+FirasBDarwish@users.noreply.github.com>
2023-07-16 20:11:05 +0000

    added network foundation

---
+++ Algorithm Design Implementations/Dynamic Programming/Maximum Flow Problem (Ford-Fullerton Algorithm).cpp
@@ -0,0 +1,10 @@
+/*
+
+This algorithm implements section 7.1.
+
+See: https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+See: https://favtutor.com/blogs/ford-fulkerson-algorithm
+
+May return to later for fun.
+
+*/
---
+++ Algorithm Design Implementations/Dynamic Programming/Maximum Flow Problem with Scaling.cpp
@@ -0,0 +1,7 @@
+/*
+
+This algorithm implements the extension from section 7.3 from Algorithm Design.
+
+I may return to it later for fun.
+
+*/
